73e49b50e9f9bd6cfab66559a672e589f4be1125
Ahmad <ahmad_baq@hotmail.com>
2022-01-11 18:49:24 +0300

    updated movies array

--- index.js
+++ index.js
@@ -54,7 +54,28 @@ console.log(movie3);
  * ]
  */
 
- const movies = [movie1, movie2, movie3];
+ const movies = [
+    {
+        id: 1,
+        name: "COCO",
+        image: "https://images.moviesanywhere.com/43e488c14820c91d7f838509bf68b529/7f534183-4d42-49d6-ad99-960e4751d053.jpg?r=3x1&w=2400",
+        rating: 7
+    }
+    
+    , {
+        id: 2,
+        name: "fight Club",
+        image: "https://cdn.domestika.org/c_limit,dpr_1.0,f_auto,q_auto,w_820/v1576552355/content-items/003/521/446/FIGHTCLUB_FINAL-original.jpg?1576552355",
+        rating: 10
+    }
+    
+    , {
+        id: 3,
+        name: "Braveheart",
+        image: "https://whatsondisneyplus.com/wp-content/uploads/2021/03/braveheart.jpg",
+        rating: 10
+    }
+ ]
 
 /** TASK 3:
  * Create a new file call it `movies.json`
